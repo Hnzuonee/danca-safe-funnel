2e8ae339bf7ea05a1680c4ccd91701d443bfda6c
Hnzuonee <harvillajan@gmail.com>
2025-04-08 20:47:59 +0200

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,1 +1,11 @@
+exports.handler = async (event, context) => {
+    const onlyFansUrl = "https://onlyfans.com/dani_mahmood";
 
+    return {
+        statusCode: 302, // Temporary redirect
+        headers: {
+            Location: onlyFansUrl
+        },
+        body: "" // No body needed for redirect
+    };
+};
