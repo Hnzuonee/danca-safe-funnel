f0f6be08c7eb59238f73c8b8331c33c010beabd2
Hnzuonee <harvillajan@gmail.com>
2025-04-18 14:39:31 +0200

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,15 +1,14 @@
 // POST‑only redirect – schová OnlyFans URL před IG botem
 exports.handler = async (event) => {
-  // 1) Povol jen POST (crawler GET dostane 405)
+  // blokuj GET (crawler)
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
-  // 2) Přesměruj reálné uživatele
   return {
     statusCode: 302,
     headers: {
-      Location: 'https://onlyfans.com/dani_mahmood', // ← změň, pokud bude jiná adresa/UTM
+      Location: 'https://onlyfans.com/dani_mahmood', // ← změň, pokud bude jiný nick/UTM
       'Cache-Control': 'no-cache'
     },
     body: ''
